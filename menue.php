6e319d2a928a2c72cc390cb5fec9536615a8cb4c
holgerkeye <holgerkeye@googlemail.com>
2014-06-16 15:15:37 +0200

    Create menue.php

---
+++ menue.php
@@ -0,0 +1,26 @@
+                	<div class="row">
+                	<div class="span12">
+                	<div class="span12">
+                  <!-- template start: navi-top -->
+                  <div class="content-navi-wrapper navbar">
+                    <div class="html5-nav content-navi-top navbar-inner"> <button
+                        data-target=".mainnav-collapse"
+                        data-toggle="collapse"
+                        class="btn btn-navbar collapsed"
+                        type="button">
+                        <span class="icon-bar"></span> <span class="icon-bar"></span>
+                        <span class="icon-bar"></span> </button>
+                      <div class="nav-collapse mainnav-collapse collapse">
+                        <ul class="nav level1">
+                          <li class="active"><a href="#">Startseite</a></li>
+                          <li><a href="#">Eingabe</a></li>
+                          <li><a href="#">Übersicht</a></li>
+                          <li><a href="#">Fehltage</a></li>
+                          <li><a href="#">Hilfe</a></li>
+                        </ul>
+                      </div>
+                      <div class="beberlin"><a href="" class="bb-logo">beBerlin</a></div>
+                    </div>
+                  </div>
+                  <!-- /template end: navi-top --> </div>
+              </div>

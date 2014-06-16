f90071cbaba2853f80c7099a54950c9c213dbf51
holgerkeye <holgerkeye@googlemail.com>
2014-06-16 15:14:47 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,28 @@
+		</div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="span12">
+            <div class="span12">
+              <!-- Beginn: Footer -->
+              <div class="html5-footer content-footer" role="contentinfo">
+                <div class="html5-nav">
+                  <ul class="nav">
+                    <li class="icon-footer icon-imprint_32"><a href="#">Impressum</a></li>
+                    <li class="icon-footer icon-information_32"><a href="#">Info</a></li>
+                    <li class="icon-footer icon-mail_32"><a href="#">Kontaktformular</a></li>
+                    <li class="right icon-footer icon-printer_32"><a href="#">Druckversion</a></li>
+                    <li class="right icon-footer icon-totop"><a href="#">zum
+                        Seitenanfang</a></li>
+                  </ul>
+                </div>
+              </div>
+              <!-- Ende: Footer --> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </body>
+</html>

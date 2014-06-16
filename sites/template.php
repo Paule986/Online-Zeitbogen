9b2e722779b44b7256db0374dfca6051ea256fbf
holgerkeye <holgerkeye@HolgersMBP.local>
2014-06-16 20:23:27 +0200

    Template nochmal rein

---
+++ sites/template.php
@@ -0,0 +1,20 @@
+<!-- Haupttemplate Online-Gleitzeitbogen -->
+<!-- Version 1.0 vom 14.06.14 von Holger Keye -->
+<!-- Header, Footer und Menu müssen im selben Verzeichnis wie template.php liegen -->
+
+<!-- Beginn: Inkludieren von Header und Menue -->
+	<?php 
+		include "../includes/header.php";
+		include "../includes/menue.php";
+	?>
+<!-- Ende: Inkludieren von Header und Menue -->
+
+<!-- Seiteninhalt -->
+             
+<!-- Seiteninhalt -->
+
+<!-- Beginn: Inkludieren von Footer -->
+<?php 
+include "../includes/footer.php"; 
+?>
+<!-- Ende: Inkludieren von Footer -->

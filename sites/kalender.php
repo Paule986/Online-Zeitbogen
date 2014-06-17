bae70d97ed403c721b72412b5320220f0c26c4d6
Tobias L. <tobias.lieschke@gmail.com>
2014-06-17 09:23:24 +0200

    Update kalender.php
    V3

--- sites/kalender.php
+++ sites/kalender.php
@@ -1,19 +1,20 @@
  <?php
-
+// Seitentitel setzen
 $seitentitel = "Kalender";
+// zusÃ¤tzliche Dateien includieren
 $zusatzinclude = "
 <script type='text/javascript' src='../includes/js/jquery.js'></script>
 <script type='text/javascript' src='../includes/js/responsive-calendar.js'></script>
 <link rel='stylesheet' href='../includes/css/responsive-calendar.css'>
 ";
-
+// header includieren
 require('../includes/header.php');
 
 // Mysql Connect Datei einbinden
 require('../includes/mysql.php');
 // MAID einlesen
 $maid = $_SESSION['maid'];
-// Timestamp fü Datumsoperationen erstellen
+// Timestamp fÃ¼ Datumsoperationen erstellen
 $timestamp = time();
 if(isset($_GET['m'])){
          $datum_monat_cal = $_GET['m'];
@@ -31,7 +32,7 @@ $datum_mon_cal = date("n",$timestamp);
 $datum_year_cal = date("Y",$timestamp);
 // Anzahl von Tagen im Monat berechnen
 $month_num = cal_days_in_month(CAL_GREGORIAN, $datum_monat_cal, $datum_year_cal);
-// String für Kalender Events erzeugen
+// String fÃ¼r Kalender Events erzeugen
 $notes="";
 // Alle Tage des Monats aufrufen, um abzufragen, ob Eintrag vorhanden
 for($tage=1;$tage<=$month_num;$tage++){
@@ -39,9 +40,9 @@ for($tage=1;$tage<=$month_num;$tage++){
          // Aus Schleifen-Wert Datumsformat erstellen
          $select_datum = "'".$datum_year_cal."-".$datum_monat_cal."-".$tag."'";
          $result = mysqli_query($link, "SELECT eid,aid FROM erfassung WHERE maid = ".$maid." AND datum = ".$select_datum.";");
-         // Anzahl an Datensätzen erfragen
+         // Anzahl an DatensÃ¤tzen erfragen
            $anzahl_notes = mysqli_num_rows($result);
-         // Wenn kein Datensatz für den aktuellen Tag --> Markierung für JS erzeugen
+         // Wenn kein Datensatz fÃ¼r den aktuellen Tag --> Markierung fÃ¼r JS erzeugen
          if($anzahl_notes<1){
 
                          $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'kein Eintrag', 'badgeClass': 'badge-warning', 'url': 'erfassung.php?maid=".$maid."&do=neu&datum=".$datum_year_cal."-".$datum_monat_cal."-".$tag."'},\n";
@@ -50,16 +51,16 @@ for($tage=1;$tage<=$month_num;$tage++){
                 while($myrow = mysqli_fetch_array($result)) {
                          // Wenn Eintrag vorhanden und AID = 3 - KRANK - dann Markierung erzeugen
                          if($myrow['aid']=="99"){
-                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'erfasst', 'class': 'normal', 'url': 'erfassung.php?maid=".$maid."&do=edit&eid=".$myrow['eid']."'},\n";
+                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'erfasst', 'class': 'normal', 'url': 'erfassung.php?do=edit&eid=".$myrow['eid']."'},\n";
                          }
                          if($myrow['aid']=="1"){
-                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'GLEITTAG', 'class': 'gleittag', 'url': 'erfassung.php?maid=".$maid."&do=edit&eid=".$myrow['eid']."'},\n";
+                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'GLEITTAG', 'class': 'gleittag', 'url': 'erfassung.php?do=edit&eid=".$myrow['eid']."'},\n";
                          }
                          if($myrow['aid']=="2"){
-                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'URL', 'class': 'urlaub', 'url': 'erfassung.php?maid=".$maid."&do=edit&eid=".$myrow['eid']."'},\n";
+                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'URL', 'class': 'urlaub', 'url': 'erfassung.php?do=edit&eid=".$myrow['eid']."'},\n";
                          }
                          if($myrow['aid']=="3"){
-                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'KRANK', 'class': 'krank', 'url': 'erfassung.php?maid=".$maid."&do=edit&eid=".$myrow['eid']."'},\n";
+                                 $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'KRANK', 'class': 'krank', 'url': 'erfassung.php?do=edit&eid=".$myrow['eid']."'},\n";
                          }
 
                  }
@@ -73,22 +74,22 @@ $notes = substr($notes, 0, -2);
      <script>
 $( document ).ready( function() {
   $(".responsive-calendar").responsiveCalendar({
-      translateMonths:["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],
+      translateMonths:["Januar","Februar","MÃ¤rz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],
     time: '<?php echo $datum_now_cal; ?>',
     startFromSunday: true,
     events: {
-      <?php  echo $notes; // Events aus Kalenderstring einfügen?>
+      <?php  echo $notes; // Events aus Kalenderstring einfÃ¼gen?>
       }
   });
 });
 </script>
 
 <!-- Responsive calendar - START -->
-<div class="responsive-calendar"  data-translate-months="Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,Novemer,Dezember" >
+<div class="responsive-calendar"  data-translate-months="Januar,Februar,MÃ¤rz,April,Mai,Juni,Juli,August,September,Oktober,Novemer,Dezember" >
   <div class="controls">
-      <a class="pull-left" href="?maid=<?php echo($maid); ?>&m=<?php echo($datum_monat_cal-1); ?>"><div class="btn"><span class="glyphicon glyphicon-backward"></span></div></a>
+      <a class="pull-left" href="?m=<?php echo($datum_monat_cal-1); ?>"><div class="btn"><span class="glyphicon glyphicon-backward"></span></div></a>
       <h4><span data-head-year></span> <span data-head-month></span></h4>
-      <a class="pull-right" href="?maid=<?php echo($maid); ?>&m=<?php echo($datum_monat_cal+1); ?>"><div class="btn"><span class="glyphicon glyphicon-forward"></span></div></a>
+      <a class="pull-right" href="?m=<?php echo($datum_monat_cal+1); ?>"><div class="btn"><span class="glyphicon glyphicon-forward"></span></div></a>
   </div><hr/>
   <div class="day-headers">
     <div class="day header">So</div>

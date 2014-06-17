80913ab1ef04135755d844852ed7807acbd842ad
Tobias L. <tobias.lieschke@gmail.com>
2014-06-17 09:35:07 +0200

    Update kalender.php

--- sites/kalender.php
+++ sites/kalender.php
@@ -1,21 +1,18 @@
  <?php
 // Seitentitel setzen
 $seitentitel = "Kalender";
-// zusätzliche Dateien includieren
+// zusÃ¤tzliche Dateien includieren
 $zusatzinclude = "
 <script type='text/javascript' src='../includes/js/jquery.js'></script>
 <script type='text/javascript' src='../includes/js/responsive-calendar.js'></script>
 <link rel='stylesheet' href='../includes/css/responsive-calendar.css'>
 ";
-// header includieren
-require('../includes/header.php');
+require('../includes/header.php'); // header includieren
+require('../includes/menue.php');  // Menü inkludieren
+require('../includes/mysql.php');  // Mysql Connect Datei einbinden
+$maid = $_SESSION['maid'];         // MAID einlesen
+$timestamp = time();               // Timestamp für Datumsoperationen erstellen
 
-// Mysql Connect Datei einbinden
-require('../includes/mysql.php');
-// MAID einlesen
-$maid = $_SESSION['maid'];
-// Timestamp fü Datumsoperationen erstellen
-$timestamp = time();
 if(isset($_GET['m'])){
          $datum_monat_cal = $_GET['m'];
          if($datum_monat_cal<=9){
@@ -32,7 +29,7 @@ $datum_mon_cal = date("n",$timestamp);
 $datum_year_cal = date("Y",$timestamp);
 // Anzahl von Tagen im Monat berechnen
 $month_num = cal_days_in_month(CAL_GREGORIAN, $datum_monat_cal, $datum_year_cal);
-// String für Kalender Events erzeugen
+// String fÃ¼r Kalender Events erzeugen
 $notes="";
 // Alle Tage des Monats aufrufen, um abzufragen, ob Eintrag vorhanden
 for($tage=1;$tage<=$month_num;$tage++){
@@ -40,9 +37,9 @@ for($tage=1;$tage<=$month_num;$tage++){
          // Aus Schleifen-Wert Datumsformat erstellen
          $select_datum = "'".$datum_year_cal."-".$datum_monat_cal."-".$tag."'";
          $result = mysqli_query($link, "SELECT eid,aid FROM erfassung WHERE maid = ".$maid." AND datum = ".$select_datum.";");
-         // Anzahl an Datensätzen erfragen
+         // Anzahl an DatensÃ¤tzen erfragen
            $anzahl_notes = mysqli_num_rows($result);
-         // Wenn kein Datensatz für den aktuellen Tag --> Markierung für JS erzeugen
+         // Wenn kein Datensatz fÃ¼r den aktuellen Tag --> Markierung fÃ¼r JS erzeugen
          if($anzahl_notes<1){
 
                          $notes .="'".$datum_year_cal."-".$datum_monat_cal."-".$tag."': {'number': 'kein Eintrag', 'badgeClass': 'badge-warning', 'url': 'erfassung.php?maid=".$maid."&do=neu&datum=".$datum_year_cal."-".$datum_monat_cal."-".$tag."'},\n";
@@ -74,18 +71,18 @@ $notes = substr($notes, 0, -2);
      <script>
 $( document ).ready( function() {
   $(".responsive-calendar").responsiveCalendar({
-      translateMonths:["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],
+      translateMonths:["Januar","Februar","MÃ¤rz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"],
     time: '<?php echo $datum_now_cal; ?>',
     startFromSunday: true,
     events: {
-      <?php  echo $notes; // Events aus Kalenderstring einfügen?>
+      <?php  echo $notes; // Events aus Kalenderstring einfÃ¼gen?>
       }
   });
 });
 </script>
 
 <!-- Responsive calendar - START -->
-<div class="responsive-calendar"  data-translate-months="Januar,Februar,März,April,Mai,Juni,Juli,August,September,Oktober,Novemer,Dezember" >
+<div class="responsive-calendar"  data-translate-months="Januar,Februar,MÃ¤rz,April,Mai,Juni,Juli,August,September,Oktober,Novemer,Dezember" >
   <div class="controls">
       <a class="pull-left" href="?m=<?php echo($datum_monat_cal-1); ?>"><div class="btn"><span class="glyphicon glyphicon-backward"></span></div></a>
       <h4><span data-head-year></span> <span data-head-month></span></h4>

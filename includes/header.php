e7e6e831a9b9d917b82fc51e5a0638aed5c52c7c
holgerkeye <holgerkeye@googlemail.com>
2014-06-20 23:28:10 +0200

    Update header.php
    Code komplett neu, weil viele Änderungen. Menü jetzt drin. (Holger Keye)

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,6 @@ session_start();
 <!DOCTYPE html>
 <html>
   <head>
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <title><?php if(isset($seitentitel)){echo $seitentitel;} ?> - Online-Gleitzeitbogen</title>
@@ -23,42 +22,63 @@ session_start();
     <link href="../includes/css/bootstrap.css" rel="stylesheet">
     <link href="../includes/css/master.css" rel="stylesheet">
     <link href="../includes/css/signin.css" rel="stylesheet">
-
+    <!-- Scripte - weiß nicht ob das gebraucht wird -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript" src="../includes/js/bootstrap.js"></script>
-
   </head>
   <body>
+  
     <!-- Beginn: Header -->
-    <div class="non-ie lang-de">
       <div class="container-wrapper container-portal-header">
         <div class="container">
           <div class="row" role="banner">
-            <div class="span12">
               <div class="html5-header portal-header">
-                <div class="html5-figure main-image"> <a href="#" title="Link f&uml;hrt zur Startseite von Berlin.de">
-                    <img class="portal-logo hide-mobile" src="../includes/images/berlin_de.png"
-                      alt="Bild zeigt: Berlin.de Logo"
-                      title="Link f&uuml;hrt zur Startseite von Berlin.de">
-                  </a> </div>
+                <div class="html5-figure main-image"> 
+                 <a href="#" title="Link f&uml;hrt zur Startseite von Berlin.de">
+                    <img class="portal-logo hide-mobile" src="../includes/images/berlin_de.png" alt="Bild zeigt: Berlin.de Logo" title="Link f&uuml;hrt zur Startseite von Berlin.de">
+                 </a> 
+                </div>
               </div>
-            </div>
           </div>
         </div>
-      </div>
-      <!-- Ende: Header -->
+      </div>     
+    <!-- Ende: Header -->
+    
       <div class="container-wrapper container-content">
-        <div class="container">
-          <!-- Beginn: Institution -->
-          <div class="row html5-header content-header" role="banner">
-            <div class="span5">
-              <div class="html5-section section-logo without-logo">
-                <div class="html5-section text"> <a href="./index.php" title="Link f&uuml;hrt zur Startseite von 'Institution Titel der Institution'">
-                    <span class="institution">Berliner Verwaltung</span> <span
-                      class="title">Online-Gleitzeitbogen</span>
-                  </a> </div>
-              </div>
-            </div>
-            <!-- Ende: Institution -->
-            <div class="row">
-              <div class="span12">
-                <div class="container">
+		<div class="container">
+		 <!-- Beginn: Institution -->
+			<div class="row html5-header content-header" role="banner">
+				<div class="html5-section section-logo without-logo">
+					<div class="html5-section text">
+						<a href="./index.php" title="Link f&uuml;hrt zur Startseite von 'Institution Titel der Institution'">
+							<span class="institution">Berliner Verwaltung</span>
+							<span class="title">Online-Gleitzeitbogen</span>
+						</a> 
+					</div>
+				</div>
+			</div>
+		 <!-- Ende: Institution -->
+		 
+		 <!-- Beginn: Menue -->
+			 <div class="row">
+
+					<div class="content-navi-wrapper navbar">
+							<div class="html5-nav content-navi-top navbar-inner">
+								<button data-target=".mainnav-collapse" data-toggle="collapse" class="btn btn-navbar collapsed" type="button"><span class="icon-bar"></span></button>
+								<div class="nav-collapse mainnav-collapse collapse">
+									<ul class="nav level1">
+														<li><a class="menu" href="../includes/test.php">Startseite</a></li>
+														<li><a class="menu" href="../sites/erfassung.php">Eingabe</a></li>
+														<li><a class="menu" href="../sites/kalender.php">Kalender</a></li>
+														<li><a class="menu" href="../sites/anzeigen.php">Liste</a></li>
+														<li><a class="menu" href="../sites/mitarbeiter.php">Mitarbeiter</a></li>
+														<li><a class="menu" href="../sites/behoerde.php">Behörden</a></li>
+														<li><a class="menu" href="#">Hilfe</a></li>
+									</ul>
+								</div>
+								<div class="beberlin"><a href="" class="bb-logo">beBerlin</a>
+							</div>
+					</div>
+
+			 </div>
+		 <!-- Ende: Menue -->

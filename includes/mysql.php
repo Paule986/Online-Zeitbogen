9e4274af6376f4b974ea026958c0676bf6018efc
Paule986 <paul.hentschke86@gmail.com>
2014-06-25 15:24:41 +0200

    Update mysql.php
    Charset auf UTF8 eingestellt, damit die Daten mit Umlauten korrekt aus der DB übernommen werden.

--- includes/mysql.php
+++ includes/mysql.php
@@ -11,5 +11,6 @@ $dbserver = "localhost";
 $link = mysqli_connect($dbserver,$dbuser,$dbpw,$datenbankname) or die("Error " . mysqli_error($link));
 // OO
 $mysqli = new mysqli($dbserver,$dbuser,$dbpw,$datenbankname)or die("Error " . mysqli_error($mysqli));;
-
+// setze Charset auf UTF-8
+mysqli_set_charset($link, 'utf8');
 ?>

0d622db274c23729496c4d89dbdd5e52dc6cdb33
Tobias L. <tobias.lieschke@gmail.com>
2014-06-26 11:28:22 +0200

    Update footer.php
    Footer leergeräumt wie besprochen

--- includes/footer.php
+++ includes/footer.php
@@ -4,11 +4,13 @@
               <div class="html5-footer content-footer" role="contentinfo">
                 <div class="html5-nav">
                   <ul class="nav">
+                    <!--
                     <li class="icon-footer icon-imprint_32"><a href="#">Impressum</a></li>
                     <li class="icon-footer icon-information_32"><a href="#">Info</a></li>
                     <li class="icon-footer icon-mail_32"><a href="#">Kontaktformular</a></li>
                     <li class="right icon-footer icon-printer_32"><a href="#">Druckversion</a></li>
                     <li class="right icon-footer icon-totop"><a href="#">zum Seitenanfang</a></li>
+                    -->
                   </ul>
                 </div>
               </div>

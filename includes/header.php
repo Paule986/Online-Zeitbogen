e87ab2e02cb2c6fdb1d30fd8fd8985166c6e404d
Tobias L. <tobias.lieschke@gmail.com>
2014-06-26 10:33:17 +0200

    Update header.php
    rundlutsching

--- includes/header.php
+++ includes/header.php
@@ -49,7 +49,7 @@ session_start();
 			<div class="row html5-header content-header" role="banner">
 				<div class="html5-section section-logo without-logo">
 					<div class="html5-section text">
-						<a href="./index.php" title="Link f&uuml;hrt zur Startseite von 'Institution Titel der Institution'">
+						<a href="dashboard.php" title="Link f&uuml;hrt zur Startseite von 'Institution Titel der Institution'">
 							<span class="institution">Berliner Verwaltung</span>
 							<span class="title">Online-Gleitzeitbogen</span>
 						</a> 

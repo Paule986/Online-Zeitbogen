48275420f5c9d52d463915be287024f95e460219
Tobias L. <tobias.lieschke@gmail.com>
2014-06-30 09:27:23 +0200

    Update footer.php
    Druckversion wieder einkommentiert; Funktion zum Drucken eingebaut

--- includes/footer.php
+++ includes/footer.php
@@ -8,9 +8,9 @@
                     <li class="icon-footer icon-imprint_32"><a href="#">Impressum</a></li>
                     <li class="icon-footer icon-information_32"><a href="#">Info</a></li>
                     <li class="icon-footer icon-mail_32"><a href="#">Kontaktformular</a></li>
-                    <li class="right icon-footer icon-printer_32"><a href="#">Druckversion</a></li>
-                    <li class="right icon-footer icon-totop"><a href="#">zum Seitenanfang</a></li>
                     -->
+                    <li class="right icon-footer icon-printer_32"><a href="javascript:window.print();">Druckversion</a></li>
+                    <li class="right icon-footer icon-totop"><a href="#">zum Seitenanfang</a></li>
                   </ul>
                 </div>
               </div>

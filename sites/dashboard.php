9c6028e706d56766276d0ab2cf57cf67e90be7d1
PhilippKw <Philippkownatka@web.de>
2014-07-03 16:00:53 +0200

    Update dashboard.php

--- sites/dashboard.php
+++ sites/dashboard.php
@@ -78,6 +78,12 @@ $monat_now_t = $monate[date("n",$timestamp)];
                                          $urlaub_ist = $result_urlaub->num_rows;
                                          $result_urlaub_max = $link->query("SELECT tage from urlaub WHERE maid='".$maid."'");
                                          $urlaub_tage = $result_urlaub_max->num_rows;
+                                         $result_tage= "SELECT urlbtage from mitarbeiter WHERE maid = '".$maid."'";
+                                         $tage_ist = mysqli_query($link, $result_tage);
+                                         while($row_tage=mysqli_fetch_array($tage_ist)){
+			 $urlaub_tage = $row_tage['tage'];
+		         }		 
+                                         
 
 echo "<table><tr><td>Geleistete Arbeitsstunden</td><td>".(round($saldo/60,1))." (".(round($saldo,0))." Min)</td></tr><tr><td>Soll-Arbeitsstunden</td><td>".($soll_std*4)." (".($soll_std*4*60)." Min)</td></tr><tr><td>Zeit-Saldo</td><td>".(round($saldo/60-($soll_std*4),1))." (".(round(($saldo-($soll_std*4*60)),0))." Min)</td></tr><tr><td>Urlaubstage</td><td>".$urlaub_tage."</td></tr><tr><td>erfasste Urlaubstage</td><td>".$urlaub_ist."</td></tr><tr><td>Krank-Tage</td><td>".$krank."</td></tr></table>";
                                 ?>

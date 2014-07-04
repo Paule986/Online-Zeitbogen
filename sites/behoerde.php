a06a4555cd6728284c2b641defa4920e080854a7
Tobias L. <tobias.lieschke@gmail.com>
2014-07-04 22:22:12 +0200

    Update behoerde.php
    Nur noch mit Adminrechte aufrufbar

--- sites/behoerde.php
+++ sites/behoerde.php
@@ -8,20 +8,21 @@ require('../includes/header.php');
 
 // mysql connect includen
 require('../includes/mysql.php');
-?>
-    <div class="container">
-	<form class="form-signin" role="form" method="post">
-    <h2 class="form-signin-heading">Bitte Behörde eintragen:</h2>
-        <input type="text" name="behoerde" id="behoerde1" class="form-control btn-kurz" placeholder="Name der Behörde" required autofocus>
-        <input type="text" name="bezirk" id="bezirk1" class="form-control btn-kurz" placeholder="Bezirk" required>
-        <input type="text" name="verantw" id="verantw1" class="form-control btn-kurz" placeholder="Verantwortlicher" required>
-		<input type="text" name="rhmzbg" id="rhmzbg1" class="form-control btn-kurz" placeholder="Rahmenzeit Beginn" required>
-		<input type="text" name="rhmzen" id="rhmzen1" class="form-control btn-kurz" placeholder="Rahmenzeit Ende" required>
-		<!--<input type="datetime-local" name="rhmzbg" id="rhmzbg1" class="form-control btn-kurz" placeholder="Rahmenzeit Beginn" required>
-		<input type="datetime-local" name="rhmzen" id="rhmzen1" class="form-control btn-kurz" placeholder="Rahmenzeit Ende" required>-->
-        <button class="btn btn-lg btn-primary btn-block  btn-kurz" type="submit" name ="eingabe_ja">Eingabe bestätigen</button>
-    </form>
-		<?php
+
+if($_SESSION["admin"]==1){
+echo"
+    <div class='container'>
+	<form class='form-signin' role='form' method='post'>
+    <h2 class='form-signin-heading'>Bitte Behörde eintragen:</h2>
+        <input type='text' name='behoerde' id='behoerde1' class='form-control btn-kurz' placeholder='Name der Behörde' required autofocus>
+        <input type='text' name='bezirk' id='bezirk1' class='form-control btn-kurz' placeholder='Bezirk' required>
+        <input type='text' name='verantw' id='verantw1' class='form-control btn-kurz' placeholder='Verantwortlicher' required>
+		<input type='text' name='rhmzbg' id='rhmzbg1' class='form-control btn-kurz' placeholder='Rahmenzeit Beginn' required>
+		<input type='text' name='rhmzen' id='rhmzen1' class='form-control btn-kurz' placeholder='Rahmenzeit Ende' required>
+		<!--<input type='datetime-local' name='rhmzbg' id='rhmzbg1' class='form-control btn-kurz' placeholder='Rahmenzeit Beginn' required>
+		<input type='datetime-local' name='rhmzen' id='rhmzen1' class='form-control btn-kurz' placeholder='Rahmenzeit Ende' required>-->
+        <button class='btn btn-lg btn-primary btn-block  btn-kurz' type='submit' name ='eingabe_ja'>Eingabe bestätigen</button>
+    </form>";
 			if (isset($_POST['eingabe_ja'])) {
 			$behoerde = $_POST["behoerde"];
 			$bezirk = $_POST["bezirk"];
@@ -32,17 +33,17 @@ require('../includes/mysql.php');
 			$eintrag = "INSERT INTO behoerde (art, name, verantwortlich, rahmenzeit_beginn, rahmenzeit_ende) VALUES ('$behoerde', '$bezirk', '$verantw', '$rhmzbg', '$rhmzen')";
 			$eintragen = mysqli_query($link, $eintrag);
 			}
-		?>
+		
 	
-		<table class="table table-striped">
+		echo"<table class='table table-striped'>
 			<tr>
 				<th>Behördenname</th>
 				<th>Bezirk</th>
 				<th>Verantwortlicher</th>
 				<th>Rahmenzeit Anfang</th>
 				<th>Rahmenzeit Ende</th>
-			</tr>
-		 <?php
+			</tr>";
+		 
 			$abfrage = "SELECT art,name,verantwortlich,rahmenzeit_beginn, rahmenzeit_ende FROM behoerde";
 			$ergebnis = mysqli_query($link, $abfrage);
 			while($row = mysqli_fetch_array($ergebnis))
@@ -55,9 +56,11 @@ require('../includes/mysql.php');
 					echo '<td>'.$row['rahmenzeit_ende'].'</td>';
 					echo '</tr>';
 					}
-		?>
-		 </table>
-	</div> 
-<?php
+		
+		 echo"</table></div>";
+}else{
+
+echo "<div class='alert alert-danger'>Sie haben keine Adminrechte !</div>";	
+}
 	require('../includes/footer.php');
 ?>

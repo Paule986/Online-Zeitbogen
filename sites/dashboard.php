8a0632a5d289547f264b891ead6ec2370a72e20d
PhilippKw <Philippkownatka@web.de>
2015-05-27 23:05:39 +0200

    Hinzufügen von Zählung Gleitz.., Auto Anpassung Soll St.
    -Anzahl Gleitzeit Tage wird ermittelt
    -Anzahl Tage Krankheit, Gleitzeit und Urlaub wird gezählt
    -daraus Stunden für diese Tage, in Berechnung Sollstunden

--- sites/dashboard.php
+++ sites/dashboard.php
@@ -98,14 +98,19 @@ $monat_now_t = $monate[$datum_monat_cal];
                                          $krank = $result_krank->num_rows;
                                          $result_urlaub = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='2' AND datum LIKE '".$datum_interval_anfang."%' ");
                                          $urlaub_ist = $result_urlaub->num_rows;
+                                         $result_gleit = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='1' AND datum LIKE '".$datum_interval_anfang."%' ");
+                                         $gleit = $result_gleit->num_rows;
                                          $result_tage= "SELECT urlbtage from mitarbeiter WHERE maid = '".$maid."'";
                                          $tage_ist = mysqli_query($link, $result_tage);
                                          while($row_tage=mysqli_fetch_array($tage_ist)){
-			 $urlaub_tage = $row_tage['urlbtage'];
-		         }		 
-                                         
-echo "<table><tr><td>Geleistete Arbeitsstunden</td><td>".(round($saldo/60,1))." (".(round($saldo,0))." Min)</td></tr><tr><td>Soll-Arbeitsstunden</td><td>".($soll_std*4)." (".($soll_std*4*60)." Min)</td></tr><tr><td>Zeit-Saldo</td><td>".(round($saldo/60-($soll_std*4),1))." (".(round(($saldo-($soll_std*4*60)),0))." Min)</td></tr><tr><td>Urlaubstage</td><td>".$urlaub_tage."</td></tr><tr><td>erfasste Urlaubstage</td><td>".$urlaub_ist."</td></tr><tr><td>Krank-Tage</td><td>".$krank."</td></tr></table>";
-                                ?>
+			 		 	$urlaub_tage = $row_tage['urlbtage'];
+		        		 }		 
+					 $gesamt= $krank + $urlaub_ist + $gleit;
+					 $abzugsoll = ($soll_std/5)*$gesamt; 
+					 $abgezogenh= ($soll_std*4)-$abzugsoll;
+					 $abgezogenmin= ($soll_std*4*60) - ($abzugsoll*60);
+					 echo "<table><tr><td><div>Geleistete Arbeitsstunden</td><td>".(round($saldo/60,1))." (".(round($saldo,0))." Min)</td></tr><tr><td>Soll-Arbeitsstunden</td><td>".($abgezogenh)." (".($abgezogenmin)." Min)</td></tr><tr><td>Zeit-Saldo</td><td>".(round($saldo/60-($abgezogenh),1))." (".(round($saldo-$abgezogenmin,0))." Min)</td></tr><tr><td>Urlaubstage</td><td>".$urlaub_tage."</td></tr><tr><td>erfasste Urlaubstage</td><td>".$urlaub_ist."</td></tr><tr><td>Krank-Tage</td><td>".$krank."</td></tr></table>";
+   			?>
                                 </div>
 <!-- Beginn: Inkludieren von Footer -->
 <?php 

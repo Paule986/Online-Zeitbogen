bd8ab6bd55d8bcbeb959147c51051ea0d7d54de0
PhilippKw <Philippkownatka@web.de>
2015-06-16 21:45:14 +0200

    Urlaubsupdate
    Jetzt werden alle Urlaubstage für das gesamte Jahr angezeigt und das Saldo aber weiterhin aus den Urlaubstagen im Monat gebildet.

--- sites/dashboard.php
+++ sites/dashboard.php
@@ -93,22 +93,31 @@ $monat_now_t = $monate[$datum_monat_cal];
                                                          $datetime2 = strtotime("1/1/1980 ".$row_sts['ende']);
                                                          $saldo=$saldo +($datetime2-$datetime1)/60;
                                         }
-                                         $saldo = round($saldo,1);
-                                         $result_krank = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='3' AND datum LIKE '".$datum_interval_anfang."%' ");
-                                         $krank = $result_krank->num_rows;
-                                         $result_urlaub = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='2' AND datum LIKE '".$datum_interval_anfang."%' ");
+                                        //Alle Urlaubstage für das Jahr
+                                         $jahr_now= date("Y");
+					 $result_urlaub = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='2' AND datum LIKE '".$jahr_now."%' ");
                                          $urlaub_ist = $result_urlaub->num_rows;
-                                         $result_gleit = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='1' AND datum LIKE '".$datum_interval_anfang."%' ");
-                                         $gleit = $result_gleit->num_rows;
-                                         $result_tage= "SELECT urlbtage from mitarbeiter WHERE maid = '".$maid."'";
+					//Anzahl erlaubter Urlaubstage
+					 $result_tage= "SELECT urlbtage from mitarbeiter WHERE maid = '".$maid."'";
                                          $tage_ist = mysqli_query($link, $result_tage);
                                          while($row_tage=mysqli_fetch_array($tage_ist)){
 			 		 	$urlaub_tage = $row_tage['urlbtage'];
 		        		 }		 
-					 $gesamt= $krank + $urlaub_ist + $gleit;
+					
+                                        //Berechnung des Saldos 
+                                         $saldo = round($saldo,1);
+                                         $result_krank = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='3' AND datum LIKE '".$datum_interval_anfang."%' ");
+                                         $krank = $result_krank->num_rows;
+                                         $result_gleit = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='1' AND datum LIKE '".$datum_interval_anfang."%' ");
+                                         $gleit = $result_gleit->num_rows;
+                                         $result_urlaub_monat = $link->query("SELECT eid FROM erfassung WHERE maid = '".$maid."' AND aid ='2' AND datum LIKE '".$datum_interval_anfang."%' ");
+                                         $urlaub_monat = $result_urlaub_monat->num_rows;
+                                         $gesamt= $krank + $urlaub_monat + $gleit;
 					 $abzugsoll = ($soll_std/5)*$gesamt; 
 					 $abgezogenh= ($soll_std*4)-$abzugsoll;
 					 $abgezogenmin= ($soll_std*4*60) - ($abzugsoll*60);
+					
+					//Ausgabe 
 					 echo "<table><tr><td><div>Geleistete Arbeitsstunden</td><td>".(round($saldo/60,1))." (".(round($saldo,0))." Min)</td></tr><tr><td>Soll-Arbeitsstunden</td><td>".($abgezogenh)." (".($abgezogenmin)." Min)</td></tr><tr><td>Zeit-Saldo</td><td>".(round($saldo/60-($abgezogenh),1))." (".(round($saldo-$abgezogenmin,0))." Min)</td></tr><tr><td>Urlaubstage</td><td>".$urlaub_tage."</td></tr><tr><td>erfasste Urlaubstage</td><td>".$urlaub_ist."</td></tr><tr><td>Krank-Tage</td><td>".$krank."</td></tr></table>";
    			?>
                                 </div>

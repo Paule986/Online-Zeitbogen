e7eb60b55d5b9c4a48fc784ff229f5914cbaf19c
steffenburlefinger <steffen.burlefinger@gmail.com>
2015-07-05 17:40:51 +0200

    Video aktualisiert
    Aktuelles Video eingebunden

--- sites/video.php
+++ sites/video.php
@@ -15,8 +15,7 @@
 <!-- Seiteninhalt -->
 	<div class="responsive-video">
     
-		<iframe width="560" height="315" src="https://player.vimeo.com/video/129083586" frameborder="0" allowfullscreen>
-        </iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/4no8NgIvC0c" frameborder="0" allowfullscreen></iframe>
         
 	</div>          
 <!-- Seiteninhalt -->
